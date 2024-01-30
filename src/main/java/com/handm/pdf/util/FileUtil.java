350359690ff869a871c0446955527bedf0b67ee1
lqm <luoqingming@fscut.com>
2024-01-30 16:24:25 +0800

    增加多类型转换

--- src/main/java/com/handm/pdf/util/FileUtil.java
+++ src/main/java/com/handm/pdf/util/FileUtil.java
@@ -3,9 +3,12 @@ package com.handm.pdf.util;
 import com.aspose.pdf.Document;
 import com.aspose.pdf.SaveFormat;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
@@ -16,17 +19,20 @@ import java.nio.charset.StandardCharsets;
 @Slf4j
 public class FileUtil {
 
+    private static final String FILENAME = "%s.%s";
+
     private FileUtil() {
         // do nothing
     }
 
-    public static void pdfToWord(HttpServletResponse response, byte[] bytes) {
-        String filename = "transform.docx";
-        Document doc = new Document(new ByteArrayInputStream(bytes));
+    public static void transformPdf(HttpServletResponse response, MultipartFile file, String type) throws IOException {
+        PDF pdf = PDF.valueOf(type.toUpperCase());
+        String filename = getFilename(file.getOriginalFilename(), pdf.getValue());
+        Document doc = new Document(new ByteArrayInputStream(file.getBytes()));
         try {
             response.setContentType("application/octet-stream");
-            response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(filename, StandardCharsets.UTF_8));
-            doc.save(response.getOutputStream(), SaveFormat.DocX);
+            response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(filename, String.valueOf(StandardCharsets.UTF_8)));
+            doc.save(filename, pdf.getSaveFormat());
         } catch (Exception e) {
             log.error("Pdf 转 Word 失败...", e);
         } finally {
@@ -34,16 +40,53 @@ public class FileUtil {
         }
     }
 
-    public static void wordToPdf(HttpServletResponse response, byte[] bytes) {
-        String filename = "transform.pdf";
+    public static void transformWord(HttpServletResponse response, MultipartFile file, String type) {
+        WORD word = WORD.valueOf(type);
+        String filename = getFilename(file.getOriginalFilename(), word.getValue());
         try {
             response.setContentType("application/octet-stream");
-            response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(filename, StandardCharsets.UTF_8));
-            com.aspose.words.Document doc = new com.aspose.words.Document(new ByteArrayInputStream(bytes));
-            doc.save(response.getOutputStream(), com.aspose.words.SaveFormat.PDF);
+            response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(filename, String.valueOf(StandardCharsets.UTF_8)));
+            com.aspose.words.Document doc = new com.aspose.words.Document(new ByteArrayInputStream(file.getBytes()));
+            doc.save(response.getOutputStream(), word.getSaveFormat());
             doc.cleanup();
         } catch (Exception e) {
             log.error("Word 转 Pdf 失败...", e);
         }
+    }
+
+    private static String getFilename(String name, String suffix) {
+        return String.format(FILENAME, name, suffix);
+    }
+
+    @Getter
+    enum PDF {
+        WORD("docx", SaveFormat.DocX),
+        EXCEL("xlsx", SaveFormat.Excel),
+        PPT("pptx", SaveFormat.Pptx),
+        IMG("", SaveFormat.Pdf);
+
+        PDF(String value, int saveFormat) {
+            this.value = value;
+            this.saveFormat = saveFormat;
+        }
+
+        private final String value;
+
+        private final int saveFormat;
+    }
+
+    @Getter
+    enum WORD {
+        PDF("pdf", com.aspose.words.SaveFormat.PDF),
+        PNG("png", com.aspose.words.SaveFormat.PNG);
+
+        WORD(String value, int saveFormat) {
+            this.value = value;
+            this.saveFormat = saveFormat;
+        }
+
+        private final String value;
+
+        private final int saveFormat;
     }
 }
